b219717411825cbc29d58dcd86ec5679fa5c1127
fanssi <fanssi911@outlook.com>
2022-03-07 09:59:10 +0800

    second push

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+namespace HelloWorldApplication
+{
+    class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            /* 我的第一个 C# 程序*/
+            Console.WriteLine("Hello World");
+            Console.ReadKey();
+        }
+    }
+}
